57dc455f199c9ccc05c5d686e6128e47c68a5170
bmax <jazvir.dhadlie@hotmail.com>
2026-05-14 20:20:01 +1000

    Finshed program which takes command line argument to create certain amount of child processes which sleep for random amount of seconds while parent is waiting for their exit.

--- Practical 8/practical8.c
+++ Practical 8/practical8.c
@@ -1,35 +1,53 @@
-#include	<stdio.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
-
+#include <sys/wait.h>
 
 int main(int argc, char* argv[]){
     int random;
-    for(int i = 0; i < 1; i++){
-        random = rand() % 10;
-        printf("Current Process PID = %d\n", getpid());
+    int status;
 
-        printf("Parent's Child PpID = %d\n", getppid());
-        sleep(random);
-
-        pid_t pid = fork();
-
-        if(pid < 0) {
-            perror("Fork failed!");
-            return -1;
+    if (argc < 1){
+    	printf("Invalid amount of arguments passed.");
+	return -1;
     }
-    if(pid == 00){
-          printf("Child Process: PID = %d\n", getpid());
-          printf("Child Process: PPID = %d\n", getppid());
-    } else {
 
-          printf("Parent Process: PID = %d\n", getpid());
-    }
+    int length = atoi(argv[1]);
+    for(int i = 0; i < length; i++){
+
     // task 1
-    fork();
-    sleep(5);
-    int ecall = 10 * i;
-    exit(ecall);
+    pid_t pid = fork();
+  
+    // error checking
+    if (pid < 0) {
+        perror("Fork Failed!");
+          return -1;
+    }
+
+    if(pid == 0){
+           printf("Child Process %d: PID = %d\n", i+1, getpid());
+           printf("Child's Parent Process %d: PPID = %d\n", i+1, getppid());
+    	   
+	   random = (rand() % 5) + 1; // randomise 2-4
+	   sleep(random);
+           int ecall = 10 * i;
+           exit(ecall);
+
+    } else {
+	   wait(&status);
+	   printf("Parent has finished waiting: Child is finished\n");
+           printf("Parent Process: PID = %d\n", getpid());
+	   if (WIFEXITED(status)) {
+		printf("Child Process %d has exited with status of %d\n\n", i+1, WEXITSTATUS(status));
+	   } else {
+		// error checking if child did not exit properly
+	   	psignal(WTERMSIG(status), "Exit Signal");
+
+	   }
+    }
+
+
+
     // use wait() or waitpid() to paause parent process until child process terminates
     // getppid() gets its process ID of its parent where getpid() gets process of current process
 
@@ -38,3 +56,4 @@ int main(int argc, char* argv[]){
     // Task 2
   }
 }
+
